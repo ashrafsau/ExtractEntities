7d1021ac5e9bdd0e5caaca7e338fb88e67235dad
ashrafsau <mdaakib18@gmail.com>
2015-11-17 16:51:13 +0530

    comments added

--- ui.R
+++ ui.R
@@ -1,13 +1,15 @@
 shinyUI(pageWithSidebar(
   headerPanel("Entity extraction from Text"),
   sidebarPanel(
+    # input text box
     textInput(inputId="text", label = "Input Text",width = '100%',placeholder = 'Enter text here'),
     actionButton("st", "Show Text"),
     actionButton("se", "Extract Entities"),
-    htmlOutput('input_text')
+    htmlOutput('input_text') # to show entered text
     
   ),
   mainPanel(
+    # to show output (extracted entities)
     dataTableOutput('output_text')
   )
 ))
